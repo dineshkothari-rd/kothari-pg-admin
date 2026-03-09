5dac09c17acf36de22ec7f7105479dbb3f3d571e
Dinesh Choudhary <dineshkothari2021@gmail.com>
2026-03-09 21:30:33 +0530

    env updated

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,15 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBZaseba3sG7aQr1W-HmVcSY7Gguembr4w",
-  authDomain: "kothari-pg-admin.firebaseapp.com",
-  projectId: "kothari-pg-admin",
-  storageBucket: "kothari-pg-admin.firebasestorage.app",
-  messagingSenderId: "124877373003",
-  appId: "1:124877373003:web:4419edb1debebd314e97ce",
-  measurementId: "G-TEWKWGJHHH",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
